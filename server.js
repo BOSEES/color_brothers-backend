956ffa08ba92da9e89f8aac454428220d61dbea5
김영일 <rladuddlf3@naver.com>
2020-12-23 18:10:02 +0900

    유저 토큰생성

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const env = require("dotenv");
 const User = require("./models/User.js");
 env.config();
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended:true
 }));
@@ -40,6 +43,36 @@ app.post("/register", (req, res) => {
       });
     }
   })
+})
+
+app.post("/login", (req, res) => {
+  //요청한 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //요청한 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+      return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+      
+      //비밀번호까지 맞다면 토큰을 생성한다.
+      user.gentoken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        //토큰을 저장한다. 어디에?? 로컬스토리지 혹은 쿠키, 세션 등등 개취
+        res.cookie("x_auth",user.token)
+        .status(200)
+        .json({loginSuccess : true , userId: user._id});
+      })
+    })
+  })
+  
+
+
 })
 
 app.listen(port, () => console.log(`Let's go color brothers http://localhost:${port}`));
